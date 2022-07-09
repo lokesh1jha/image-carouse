33985197548bc88d7289d20eee61770145bec184
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-07-09 21:57:51 +0530

    image-carouse
    a horizontal slide of images

---
+++ script.js
@@ -0,0 +1,17 @@
+const imgs = document.getElementById('imgs');
+
+let idx = 0;
+
+const img = document.querySelectorAll('#imgs img');
+
+function run() {
+    idx++;
+
+    if(idx > img.length-1){
+       idx = 0; 
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(run, 2000);
